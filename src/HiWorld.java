19287a1b26021808975bcb6a42a8c5b485e93719
TinyClock <609132714@qq.com>
2019-12-06 15:32:27 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: Chuansheng Zhong
+ * @create: 2019-12-06 15:29
+ **/
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World!");
+    }
+}
